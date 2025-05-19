a06aaa36ae175add54f87264c29c59256916c7c4
f-pinkus <faigypinkus@gmail.com>
2025-05-19 17:00:44 -0400

    dynamic searches

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,26 +5,8 @@ class ProductsController < ApplicationController
     render json: products
   end
 
-  def product1
-    product = Product.find(1)
-
-    render json: product
-  end
-
-  def product2
-    product = Product.find(2)
-
-    render json: product
-  end
-
-  def product3
-    product = Product.find(3)
-
-    render json: product
-  end
-
-  def product4
-    product = Product.find(4)
+  def show
+    product = Product.find(params[:id])
 
     render json: product
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   get "/allproducts", controller: "products", action: "all_products"
-  get "/product1", controller: "products", action: "product1"
-  get "/product2", controller: "products", action: "product2"
-  get "/product3", controller: "products", action: "product3"
-  get "/product4", controller: "products", action: "product4"
+  get "/show/:id", controller: "products", action: "show"
 end
