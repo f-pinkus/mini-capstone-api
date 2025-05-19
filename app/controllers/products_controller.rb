92a4b903dd77a0938547057b96ec1f1c9590a14a
f-pinkus <faigypinkus@gmail.com>
2025-05-19 16:25:18 -0400

    diff gets and actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,28 @@ class ProductsController < ApplicationController
 
     render json: products
   end
+
+  def product1
+    product = Product.find(1)
+
+    render json: product
+  end
+
+  def product2
+    product = Product.find(2)
+
+    render json: product
+  end
+
+  def product3
+    product = Product.find(3)
+
+    render json: product
+  end
+
+  def product4
+    product = Product.find(4)
+
+    render json: product
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/allproducts", controller: "products", action: "all_products"
+  get "/product1", controller: "products", action: "product1"
+  get "/product2", controller: "products", action: "product2"
+  get "/product3", controller: "products", action: "product3"
+  get "/product4", controller: "products", action: "product4"
 end
