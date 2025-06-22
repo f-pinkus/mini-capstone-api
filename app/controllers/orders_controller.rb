05c009c68abb6d48ac631a0543a9f290e15e520e
f-pinkus <faigypinkus@gmail.com>
2025-06-22 14:27:00 -0400

    authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,41 +1,49 @@
 class OrdersController < ApplicationController
   def index
-    @orders = current_user.orders
+    if current_user
+      @orders = current_user.orders
 
-    render json: @orders
+      render json: @orders
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 
   def create
-    product = Product.find(params[:product_id])
+    if current_user
 
-    subtotal = product.price * params[:quantity].to_i
-    tax = subtotal * 0.08
-    total = tax + subtotal
-    
-    @order = Order.new(
-      user_id: current_user.id, 
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: subtotal,
-      tax: tax,
-      total: total
-    )
+      product = Product.find(params[:product_id])
 
-    if @order.save
-      render json: @order, status: :created
+      subtotal = product.price * params[:quantity].to_i
+      tax = subtotal * 0.08
+      total = tax + subtotal
+      
+      @order = Order.new(
+        user_id: current_user.id, 
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: subtotal,
+        tax: tax,
+        total: total
+      )
+
+      if @order.save
+        render json: @order, status: :created
+      else
+        render json: { errors: @order.errors.full_messages }, status: :bad_request
+      end
     else
-      render json: { errors: @order.errors.full_messages }, status: :bad_request
+      render json: {}, status: :unauthorized
     end
   end
 
   def show
-    @order = current_user.orders.find_by(id: params[:id])
+    if current_user
+      @order = current_user.orders.find_by(id: params[:id])
 
-    if @order
       render json: @order
     else
-      render json: { message: "Not your order!" }
+      render json: {}, status: :unauthorized
     end
   end
-
 end
