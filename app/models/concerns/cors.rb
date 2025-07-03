c3cd6ddb91f9b815fced7ecf325a0ea26e79421c
f-pinkus <faigypinkus@gmail.com>
2025-07-03 17:33:51 -0400

    fix cors

--- app/models/concerns/cors.rb
+++ app/models/concerns/cors.rb
@@ -1,9 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://mini-capstone.peterxjang.com"
-    resource "*",
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
+    origins "mini-capstone.peterxjang.com", "localhost:5173", "demo-mini-capstone-frontend-mgoi.onrender.com"
+    resource "*", headers: :any, credentials: true, methods: [:get, :post, :patch, :put, :delete]
   end
-end 
+end
