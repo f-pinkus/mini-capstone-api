99068e1964970030a55fccdeb71db650e2ebe2e9
f-pinkus <faigypinkus@gmail.com>
2025-07-03 16:55:03 -0400

    fix models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,42 +1,52 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 2} 
-  validates :price, presence: true, numericality: true,   numericality: { greater_than: 0 } 
-  validates :description, length: { maximum: 500 }
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 1..5000 }
 
-  belongs_to :supplier
-  has_many :images
   has_many :category_products
   has_many :categories, through: :category_products
+  # def categories
+  #   category_products.map do |category_product|
+  #     Category.find_by(id: category_product.category_id)
+  #   end
+  # end
 
-  def all_images
-    images.each do |image|
-      return image.url
-    end
-  end
-  
-  def friendly_created_at
-    created_at.strftime("%A, %d %b %Y %l:%M %p")
-  end
+  has_many :carted_products
+  has_many :orders, through: :carted_products
+
+  TAX_RATE = 0.09
 
   def is_discounted?
-    if price <= 4
-      return true
-    else
-      return false
-    end
+    price <= 10
   end
 
   def tax
-    price * 0.09
+    price * TAX_RATE
   end
 
   def total
     price + tax
   end
 
-  # def categories
-  #   category_products.map do |category_product|
-  #     category_product.category
-  #   end
+  belongs_to :supplier
+  # validates :supplier_id, presence: true
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
   # end
+
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
+
+  def primary_image_url
+    if images.length > 0
+      images[0].url
+    else
+      "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+    end
+  end
 end
