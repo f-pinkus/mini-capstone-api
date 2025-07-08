21843bf82cd296cc519e9d2ae3f0d8db15f15175
f-pinkus <faigypinkus@gmail.com>
2025-07-08 11:52:27 -0400

    fix cors

--- app/models/concerns/cors.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "https://mini-capstone.peterxjang.com", "http://localhost:5173", "<https://theclothingco.netlify.app/"
-    resource "*",
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
-  end
-end
